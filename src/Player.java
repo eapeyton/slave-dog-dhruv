540585bf2bd722200ce6079779e0c7c476cccedb
Dhruv Saksena <saksenad@gatech.edu>
2012-10-08 20:30:43 -0400

    Added player Class

---
+++ src/Player.java
@@ -0,0 +1,26 @@
+/**
+ * Represents the actual Player
+ * 
+ * @author Dhruv Saksena
+ *@version M5
+ */
+public class Player {
+
+	String name;
+	int pilotp;
+	int fighterp;
+	int traderp;
+	int enggp;
+	
+	public Player(String name, int pilotp, int fighterp, int traderp, int enggp) {
+		super();
+		this.name = name;
+		this.pilotp = pilotp;
+		this.fighterp = fighterp;
+		this.traderp = traderp;
+		this.enggp = enggp;
+	}
+	
+	
+	
+}
