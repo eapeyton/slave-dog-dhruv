2e2c5cdddab677035a45dfb10ac9ea8d60d3e814
emorphis <emorphis3@gatech.edu>
2012-10-21 22:36:51 -0400

    Eliminated risk of overlapping locations, increased scarcity of special resources, javadoc'd

--- src/StarSystem.java
+++ src/StarSystem.java
@@ -1,33 +1,53 @@
+import java.awt.Point;
+import java.util.HashSet;
 
+/**
+ * Holds information about a star system. Used by Galactic chart to add star systems 
+ * to the universe.
+ * @author Dhruv Saksena, Eric Morphis
+ *
+ */
 public class StarSystem {
 
 	String name;
 	int xMap;
 	int yMap;
+	Point location;
 	int techLevel;
 	int resources;
 	int govt;
+	static HashSet<Point> usedLocations = new HashSet<Point>();
 	final int screenWidth=150;
 	final int screenHeight=100;
 	
+	/**
+	 * Creates a new star system with a unique location
+	 * @param name The name of the star system
+	 */
 	public StarSystem(String name) {
 		super();
 		this.name=name;
-		xMap=(int) (Math.random()*screenWidth);
-		yMap=(int) (Math.random()*screenHeight);
+		while (location == null || usedLocations.contains(location)) {
+			setLocation();
+		}
+		usedLocations.add(location);
 		techLevel=(int) (Math.random()*8);
-		resources=(int) (Math.random()*13);
+		resources=(int) ((Math.random()*13)*(Math.floor(Math.random()*2)));
 		govt=(int) (Math.random()*18);
 		
 	}
 	
-	public void relocate(){//Incase 2 StarSystems overlap
+	/**
+	 * Randomly selects a location for the star system
+	 */
+	public void setLocation(){//Incase 2 StarSystems overlap
 		xMap=(int) (Math.random()*screenWidth);
 		yMap=(int) (Math.random()*screenHeight);
+		location = new Point(xMap, yMap);
 	}
 	
 	/**
-	 * Rewrite toString method to display name and attributes.
+	 * @return The star system's name and attributes as a string
 	 */
 	public String toString() {
 		
